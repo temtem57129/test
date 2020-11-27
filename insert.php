7a65fb394f8d16e1a5ca4d88381a1c910c716145
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-27 10:02:25 +0700

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,27 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'kantawit11.mysql.database.azure.com', 'kantawit123@kantawit11', 'OGUlot28', 'ITFlab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$product = $_POST['product'];
+$price = $_POST['price'];
+$amount = $_POST['amount'];
+$total = $_POST['price*amont'];
+
+
+$sql = "INSERT INTO test (product , price , amount, total) VALUES ('$product', '$price', '$amount', '$total' )";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+mysqli_close($conn);
+?>
