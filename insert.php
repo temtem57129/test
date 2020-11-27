def51467cfe29da1a8c375f2e21a9b456ba8b7ed
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-27 10:22:51 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -21,6 +21,11 @@ if (mysqli_query($conn, $sql)) {
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
+<html>
+<p>
+<a href="https://kantawit.azurewebsites.net/">Back to Show</a>
+</body>
+</html>
 
 mysqli_close($conn);
 ?>
